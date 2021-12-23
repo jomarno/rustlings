36ca18907395d05fdffccbcd849430f14a460996
J MN <jomarno530.git@gmail.com>
2021-12-23 14:52:06 +0100

    Skipped clippy1.rs because Clippy doesn't raise any errors, solved clippy2.rs, added clippy/target/ to .gitignore

--- clippy/clippy1.rs
+++ clippy/clippy1.rs
@@ -6,8 +6,9 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
+// // I AM NOT DONE
 
+// SOLUTION: Nothing changed because clippy doesn't raise any errors
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
--- clippy/clippy2.rs
+++ clippy/clippy2.rs
@@ -1,12 +1,13 @@
 // clippy2.rs
 // Make me compile! Execute `rustlings hint clippy2` for hints :)
 
-// I AM NOT DONE
+// // I AM NOT DONE
 
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
+    // SOLUTION: Replaced for loop with if let statement in following line
+    if let Some(x) = option {
         res += x;
     }
     println!("{}", res);
