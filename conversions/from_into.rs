38da2f29ee797e267d940642d3e6dbedca5f6040
J MN <jomarno530.git@gmail.com>
2022-11-04 11:00:47 +0100

    Added comment to from_into.rs solution

--- conversions/from_into.rs
+++ conversions/from_into.rs
@@ -33,10 +33,10 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+// // I AM NOT DONE
 
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {
+    fn from(s: &str) -> Person { // SOLUTION: Wrote body of function
         if s.len() == 0 {
             return Person::default();
         } else {
